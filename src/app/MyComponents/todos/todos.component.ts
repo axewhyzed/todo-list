68d8842b44801676d9f8474e52b48f335a84d590
axewhyzed <coding0234@gmail.com>
2024-01-03 11:56:23 +0530

    Created Todo Component

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
-export class TodosComponent {
+export class TodosComponent{
+  todos:Todo[] = [];
 
+  constructor(){
+    this.todos = [
+      {
+        sno:1,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:2,
+        title: "This is title 2",
+        desc: "Description 2",
+        active: true
+      },
+      {
+        sno:3,
+        title: "This is title 3",
+        desc: "Description 3",
+        active: true
+      },
+    ]
+  }
 }
