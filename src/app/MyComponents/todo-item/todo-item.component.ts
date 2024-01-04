6c29062dc85dfa6ae760e4e8289bfdd13aae75ba
axewhyzed <coding0234@gmail.com>
2024-01-04 15:40:04 +0530

    added mark as done feature

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -1,19 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../../Todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css'
 })
 export class TodoItemComponent {
   @Input() todo!: Todo;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
 
   onClick(todo: Todo){
     this.todoDelete.emit(todo);
     console.log("onClick has been clicked");
+  }
+
+  onCheckboxClick(todo: Todo){
+    this.todoCheckbox.emit(todo);
   }
 }
